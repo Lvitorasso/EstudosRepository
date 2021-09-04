1170de0167d5f68c0fb927b0f83269579ac3e674
Lucas vitorasso <lucasvitorasso@hotmail.com>
2021-09-04 10:28:15 -0300

    Erros

--- PrimeiroAppAngular/Angular/src/app/Service/post.service.ts
+++ PrimeiroAppAngular/Angular/src/app/Service/post.service.ts
@@ -1,4 +1,6 @@
-import { AppError } from '../Compartilhado/Erros/app-error';
+import { BadRequestError } from './../Compartilhado/Erros/bad-request-error';
+import { AppError } from './../Compartilhado/Erros/app-error';
+import { NotFoundError } from './../Compartilhado/Erros/not-found-error';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map } from 'rxjs/operators';
@@ -19,24 +21,53 @@ export class PostService {
   }
 
   getPosts(){
-    return this.httpClient.get(this.url);
+    return this.httpClient.get(this.url).pipe(
+      catchError((error: Response) => {      
+        if(error.status == 404)
+          return throwError(new NotFoundError(error.json())); 
+        else if(error.status == 400)
+          return throwError(new BadRequestError(error.json()));
+        else          
+          return throwError(new AppError(error.json()));
+     }));
   }
 
   createPost(post: any)
   {
-    return this.httpClient.post(this.url, JSON.stringify(post));
+    return this.httpClient.post(this.url, JSON.stringify(post)).pipe(
+      catchError((error: Response) => {     
+        if(error.status == 404)
+          return throwError(new NotFoundError(error.json())); 
+        else if(error.status == 400)
+          return throwError(new BadRequestError(error.json()));
+        else          
+          return throwError(new AppError(error.json()));
+     }));
   }
 
   updatePost(post: any)
   {
-    return this.httpClient.patch(this.url+'/'+post.id, JSON.stringify({isRead: true}));
+    return this.httpClient.patch(this.url+'/'+post.id, JSON.stringify({isRead: true})).pipe(
+      catchError((error: Response) => {      
+        if(error.status == 404)
+          return throwError(new NotFoundError(error.json())); 
+        else if(error.status == 400)
+          return throwError(new BadRequestError(error.json()));
+        else          
+          return throwError(new AppError(error.json()));
+     }));
   }
 
   deletePost(id: any)
   {
     return this.httpClient.delete(this.url+'/'+id).pipe(
       catchError((error: Response) => {      
-          return throwError(new AppError());       
+        if(error.status == 404)
+          return throwError(new NotFoundError(error.json())); 
+        else if(error.status == 400)
+          return throwError(new BadRequestError(error.json()));
+        else          
+          return throwError(new AppError(error.json()));
      }));
   }
 
