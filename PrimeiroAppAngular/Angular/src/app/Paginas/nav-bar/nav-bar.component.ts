f6bbd6761cb685a77e1c1cbd9a4c645469cbec86
Lucas vitorasso <lucasvitorasso@hotmail.com>
2021-10-01 09:37:18 -0300

    ultimas atualizações

--- PrimeiroAppAngular/Angular/src/app/Paginas/nav-bar/nav-bar.component.ts
+++ PrimeiroAppAngular/Angular/src/app/Paginas/nav-bar/nav-bar.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav-bar',
+  selector: 'navbarLoja',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit 
+{
+
+  usuario = "Lucas";
+
+  imagePath = 'C:\Users\Lucas\source\repos\Lvitorasso\EstudosRepository\PrimeiroAppAngular\Angular\src\app\images\brushlogo.png';
 
   constructor() { }
 
